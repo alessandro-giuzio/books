58049c76f38dcdbc8de14042918752380709fe61
alessandro-giuzio <giuzio@icloud.com>
2024-08-19 16:20:17 +0200

    agosto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,44 +1,123 @@
-const defaulTheme = require('tailwindcss/defaultTheme');
+import animate from "tailwindcss-animate";
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-height:{
-	'base':'116rem',
-	'base2':'120rem',
-},
-			colors:{
-				midnightMystery: '#13004e',
-				royalIndigo: '#48007e',
-				electricAmethyst: '#7f00b1',
-				whisperingLilac: '#FBF0FF',
-				sunsetGold: '#F0A000',
-				forestDepths: '#4c6800',
-				neonLime: '#c0ff14',
-				buttercreamCream: '#f1ffcc',
-				softPear: '#f8ffe5',
-				tropicalSky:'#05bcff',
-				bubblegumBlast:'#ff05bc',
-				background: '#1E2237',
-				borderGolden: '#F0A000',
-				borderSilver: '#C0C0C0',
-				borderBronze: '#CD7F32',
+  darkMode: ["class"],
+  safelist: ["dark"],
+  prefix: "",
 
-			},
-			fontFamily: {
-        sans: ['Tourney Variable', ...defaulTheme.fontFamily.sans],
-				mono: ['"IBM Plex Mono"', ...defaulTheme.fontFamily.mono],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
-		},
-	},
-	daisyui: {
-		themes: ["light", "dark", "cupcake","bumblebee",
-		"emerald",
-		"corporate",
-		"synthwave",
-		"retro",
-		"cyberpunk",],
-	},
-	plugins: [require("daisyui")],
-}
+    },
+    extend: {
+      colors: {
+        midnightMystery: '#13004e',
+        royalIndigo: '#48007e',
+        electricAmethyst: '#7f00b1',
+        whisperingLilac: '#FBF0FF',
+        sunsetGold: '#F0A000',
+        forestDepths: '#4c6800',
+        neonLime: '#c0ff14',
+        buttercreamCream: '#f1ffcc',
+        softPear: '#f8ffe5',
+        tropicalSky:'#05bcff',
+        bubblegumBlast:'#ff05bc',
+        background: '#1E2237',
+        borderGolden: '#F0A000',
+        borderSilver: '#C0C0C0',
+        borderBronze: '#CD7F32',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ['Tourney Variable', ...defaultTheme.fontFamily.sans],
+        mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
+      },
+      daisyui: {
+        themes: [
+          "light", "dark", "cupcake", "bumblebee",
+          "emerald", "corporate", "synthwave", "retro", "cyberpunk",
+        ],
+      },
+      height: {
+        'base': '116rem',
+        'base2': '120rem',
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        "collapsible-down": {
+          from: { height: 0 },
+          to: { height: 'var(--radix-collapsible-content-height)' },
+        },
+        "collapsible-up": {
+          from: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-in-out",
+        "collapsible-up": "collapsible-up 0.2s ease-in-out",
+      },
+    },
+  },
+  plugins: [
+    require("daisyui"),
+    animate,
+  ],
+};
