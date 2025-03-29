877130aee31a35d95d3199f1ab5f6d92ce523af3
Alessandro Giuzio <38163055+alessandro-giuzio@users.noreply.github.com>
2025-03-29 17:25:26 +0100

    feat: improve layout and styling of AddBookForm and BookOfTheDay components

--- src/components/dashboard/BookOfTheDay.vue
+++ src/components/dashboard/BookOfTheDay.vue
@@ -1,49 +1,30 @@
 <template>
-  <picture>
-    <!-- Smallest image for small devices -->
-    <source
-      :srcset="bookImage320.src"
-      media="(max-width: 480px)"
-      alt="books in space"
-      class="rounded-lg shadow-lg mx-auto w-full" />
+  <section class="text-center px-4 max-w-4xl mx-auto py-8">
+    <h1
+      class="text-3xl sm:text-4xl md:text-5xl font-semibold uppercase tracking-tight text-basque-40 mb-6">
+      Eguneko liburua
+    </h1>
 
-    <!-- Medium image for tablets -->
-    <source
-      :srcset="bookImage640.src"
-      media="(max-width: 768px)"
-      alt="books in space"
-      class="rounded-lg shadow-lg mx-auto w-full" />
+    <picture>
+      <source :srcset="bookImage320.src" media="(max-width: 480px)" />
+      <source :srcset="bookImage640.src" media="(max-width: 768px)" />
+      <source :srcset="bookImage1200.src" media="(max-width: 1440px)" />
+      <source :srcset="bookImage1920.src" media="(min-width: 1441px)" />
+      <img
+        :src="books"
+        alt="Books floating in space"
+        class="rounded-lg shadow-lg mx-auto w-full max-w-4xl"
+        loading="lazy" />
+    </picture>
 
-    <!-- Larger image for desktops -->
-    <source
-      :srcset="bookImage1200.src"
-      media="(max-width: 1440px)"
-      alt="books in space"
-      class="rounded-lg shadow-lg mx-auto w-full" />
-
-    <!-- Largest image for large desktops -->
-    <source
-      :srcset="bookImage1920.src"
-      media="(min-width: 1441px)"
-      alt="books in space"
-      class="rounded-lg shadow-lg mx-auto w-full" />
-    <!-- Fallback -->
-    <img
-      :src="books"
-      alt="books in space"
-      class="rounded-lg shadow-lg mx-auto w-full"
-      loading="lazy" />
-  </picture>
-  <h1
-    class="text-5xl font-semibold uppercase text-center tracking-tight text-basque-40">
-    Eguneko liburua
-  </h1>
-  <div class="mt-6 p-4 border border-gray-800 rounded bg-gray-800">
-    <h3 class="text-2xl font-bold shadow-xl text-basque-40 mb-2">
-      {{ title }}
-    </h3>
-    <p class="text-sunsetGold text-xl mb-0">by {{ author }}</p>
-  </div>
+    <div class="mt-6 p-4 rounded bg-gray-800 border border-gray-700">
+      <h3
+        class="text-2xl font-bold text-basque-40 mb-2 transition-transform duration-800 hover:scale-105">
+        {{ title }}
+      </h3>
+      <p class="text-sunsetGold text-xl">by {{ author }}</p>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
